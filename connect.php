8a154189ab1c574d81be6c68870b08d6641cd633
Daryll Bobis <112360348+DAB123123@users.noreply.github.com>
2024-06-24 23:40:19 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,17 +1,13 @@
 <?php
-// Database connection settings
 $servername = "attendancemsystem.cxueocagesiq.ap-southeast-2.rds.amazonaws.com";
 $username = "admin";
 $password = "October92002";
-$dbname = "attsystem";
 
-// Establishing connection with the RDS database
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=attsystem", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
-
-echo "Connected successfully";
 ?>
