83267f689e3b92849104dc549848c1c0205a89c2
Daryll Bobis <112360348+DAB123123@users.noreply.github.com>
2024-06-25 00:41:33 +0800

    Update students.php

--- student/students.php
+++ student/students.php
@@ -1,119 +1,100 @@
 <?php
-
 ob_start();
 session_start();
 
-if($_SESSION['name']!='oasis')
-{
+if($_SESSION['name']!='oasis') {
   header('location: login.php');
+  exit(); // Always exit after header redirect
 }
 ?>
-<?php include('connect.php');?>
+<?php include('connect.php'); ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Online Attendance Management System 1.0</title>
-<meta charset="UTF-8">
+  <title>Online Attendance Management System 1.0</title>
+  <meta charset="UTF-8">
   
   <link rel="stylesheet" type="text/css" href="../css/main.css">
   <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
    
   <!-- Optional theme -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
    
-  <link rel="stylesheet" href="styles.css" >
+  <link rel="stylesheet" href="styles.css">
    
   <!-- Latest compiled and minified JavaScript -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-
 </head>
 <body>
 
 <header>
-
   <h1>Online Attendance Management System 1.0</h1>
   <div class="navbar">
-  <a href="index.php">Home</a>
-  <a href="students.php">Students</a>
-  <a href="report.php">My Report</a>
-  <a href="account.php">My Account</a>
-  <a href="../logout.php">Logout</a>
-
-</div>
-
+    <a href="index.php">Home</a>
+    <a href="students.php">Students</a>
+    <a href="report.php">My Report</a>
+    <a href="account.php">My Account</a>
+    <a href="../logout.php">Logout</a>
+  </div>
 </header>
 
 <center>
-
 <div class="row">
-
   <div class="content">
     <h3>Student List</h3>
     <br>
 
-   <form method="post" action="" class="form-horizontal col-md-6 col-md-offset-3">
+    <form method="post" action="" class="form-horizontal col-md-6 col-md-offset-3">
       <div class="form-group">
-          <label for="input1" class="col-sm-3 control-label">Batch</label>
-            <div class="col-sm-7">
-                <input type="text" name="sr_batch"  class="form-control" id="input1" placeholder="Only 2020" />
-                
-            </div>
-
+        <label for="input1" class="col-sm-3 control-label">Batch</label>
+        <div class="col-sm-7">
+          <input type="text" name="sr_batch" class="form-control" id="input1" placeholder="Only 2020" />
+        </div>
       </div>
       <input type="submit" class="btn btn-primary col-md-3 col-md-offset-7" value="Go!" name="sr_btn" />
-      
-   </form>
+    </form>
 
-   <div class="content"></div>
+    <div class="content"></div>
     <table class="table table-striped">
-      
       <thead>
-      <tr>
-        <th scope="col">Registration No.</th>
-        <th scope="col">Name</th>
-        <th scope="col">Department</th>
-        <th scope="col">Batch</th>
-        <th scope="col">Semester</th>
-        <th scope="col">Email</th>
-      </tr>
+        <tr>
+          <th scope="col">Registration No.</th>
+          <th scope="col">Name</th>
+          <th scope="col">Department</th>
+          <th scope="col">Batch</th>
+          <th scope="col">Semester</th>
+          <th scope="col">Email</th>
+        </tr>
       </thead>
-   <?php
-
-    if(isset($_POST['sr_btn'])){
-     
-     $srbatch = 2020;
-     $i=0;
-     
-     $all_query = mysql_query("select * from students where students.st_batch = '$srbatch' order by st_id asc");
-     
-     while ($data = mysql_fetch_array($all_query)) {
-       $i++;
-     
-     ?>
-
-     <tr>
-       <td><?php echo $data['st_id']; ?></td>
-       <td><?php echo $data['st_name']; ?></td>
-       <td><?php echo $data['st_dept']; ?></td>
-       <td><?php echo $data['st_batch']; ?></td>
-       <td><?php echo $data['st_sem']; ?></td>
-       <td><?php echo $data['st_email']; ?></td>
-     </tr>
-
-     <?php 
-          } 
-              }
-      ?>
+      <tbody>
+        <?php
+        if(isset($_POST['sr_btn'])) {
+          $srbatch = $_POST['sr_batch'];
+          $stmt = $conn->prepare("SELECT * FROM students WHERE st_batch = :srbatch ORDER BY st_id ASC");
+          $stmt->bindParam(':srbatch', $srbatch);
+          $stmt->execute();
+          $i = 0;
+          while ($data = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $i++;
+            ?>
+            <tr>
+              <td><?php echo $data['st_id']; ?></td>
+              <td><?php echo $data['st_name']; ?></td>
+              <td><?php echo $data['st_dept']; ?></td>
+              <td><?php echo $data['st_batch']; ?></td>
+              <td><?php echo $data['st_sem']; ?></td>
+              <td><?php echo $data['st_email']; ?></td>
+            </tr>
+            <?php
+          }
+        }
+        ?>
+      </tbody>
     </table>
-
   </div>
-
 </div>
-
 </center>
-
 </body>
 </html>
