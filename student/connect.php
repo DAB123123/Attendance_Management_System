210ed3802d90df3e0ef184a2fa4c76f3414c1cff
Daryll Bobis <112360348+DAB123123@users.noreply.github.com>
2024-06-24 21:16:08 +0800

    Update connect.php

--- student/connect.php
+++ student/connect.php
@@ -1,6 +1,17 @@
 <?php
+// Database connection settings
+$servername = "attendancemsystem.cxueocagesiq.ap-southeast-2.rds.amazonaws.com";
+$username = "admin";
+$password = "October92002";
+$dbname = "attsystem";
 
-mysql_connect('localhost','root','') or die('Cannot connect to server');
-mysql_select_db('attsystem') or die ('Cannot found database');
+// Establishing connection with the RDS database
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "Connected successfully";
 ?>
